01a04bf415f0c32aae1fb27bc508dcc2fbf8f1da
manuvikn <manuelvictoriacode@gmail.com>
2021-10-25 18:03:49 +0200

    implementación de persistencia de tareas

--- src/js/inquirer.js
+++ src/js/inquirer.js
@@ -9,31 +9,31 @@ const menuOpts = [
         choices: [
             {
                 value: '1',
-                name: '1. Crear tarea'
+                name: `${'1.'.green} Crear tarea`
             },
             {
                 value: '2',
-                name: '2. Listar tareas'
+                name: `${'2.'.green} Listar tareas`
             },
             {
                 value: '3',
-                name: '3. Listar tareas completadas'
+                name: `${'3.'.green} Listar tareas completadas`
             },
             {
                 value: '4',
-                name: '4. Listar tareas pendientes'
+                name: `${'4.'.green} Listar tareas pendientes`
             },
             {
                 value: '5',
-                name: '5. Completar tarea(s)'
+                name: `${'5.'.green} Completar tarea(s)`
             },
             {
                 value: '6',
-                name: '6. Borrar tarea'
+                name: `${'6.'.green} Borrar tarea`
             },
             {
                 value: '0',
-                name: '0. Salir'
+                name: `${'0.'.red} Salir`
             }
         ]
     }
@@ -41,9 +41,9 @@ const menuOpts = [
 
 const inquirerMenu = async () => {
     console.clear();
-    console.log('=========================='.yellow);
-    console.log('  Seleccione una opción  '.yellow);
-    console.log('==========================\n'.yellow);
+    console.log('========================='.green);
+    console.log('  Seleccione una opción  ');
+    console.log('=========================\n'.green);
 
     const {opt} = await inquirer.prompt(menuOpts);
 
@@ -61,7 +61,28 @@ const pause = async () => {
 
 };
 
+const readInput = async (message) => {
+
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate(value) {
+                if (value.length === 0 ) {
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+        }
+    ];
+
+    const {desc} = await inquirer.prompt(question);
+    return desc;
+};
+
 module.exports = {
     inquirerMenu,
-    pause
+    pause,
+    readInput
 };
--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,9 +1,46 @@
+const Task = require("./task");
+const fs = require('fs');
+
+const dirbase = `${__dirname + '/../db/database.json'}`;
+
 class Tasks {
 
     tasks = {};
 
     constructor() {
-        this.tasks = {};
+        this.loadTasks();
+    }
+
+    newTask(desc) {
+
+        const task = new Task(desc);
+        this.tasks[task.id] = task;
+
+    }
+
+    get tasksArr() {
+
+        const tasks = [];
+        Object.keys(this.tasks).forEach(key => tasks.push(this.tasks[key]));
+        return tasks;
+
+    }
+
+    loadTasks() {
+
+        if (fs.existsSync(dirbase)) {
+            const data = fs.readFileSync(dirbase, {encoding: 'utf-8'});
+            JSON.parse(data).forEach(item => {
+                this.tasks[item['id']] = item;
+            });
+        }
+
+    }
+
+    saveTasks() {
+    
+        fs.writeFileSync(dirbase, JSON.stringify(this.tasksArr));
+
     }
 
 }
